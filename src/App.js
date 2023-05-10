15d99b1c12993eee45fbe3859558b764d3837449
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-05-10 15:38:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-
 //import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import Home from './components/Home';
+import ShowSummary from './components/ShowSummary';
 
 
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
 } from 'react-router-dom';
-import ShowSummary from './components/ShowSummary';
 
-//
+
 
 function App() {
-  
-    return (
-      <div>
-      
-       <Router>
-           <div className="App">
-           {/* <ul>
+
+  return (
+    <div>
+
+      <Router>
+        <div className="App">
+          {/* <ul>
            <li>
         <Link to="/Home">Home</Link>
       </li>
@@ -31,17 +30,17 @@ function App() {
       </li>
     
     </ul> */}
-    <Routes>
-    <Route exact path='/Home' element={< Home />}></Route>
-                 <Route exact path='/ShowSummary' element={< ShowSummary />}></Route>
-                 
+          <Routes>
+            <Route exact path='/Home' element={< Home />}></Route>
+            <Route exact path='/ShowSummary' element={< ShowSummary/>}></Route>
+
           </Routes>
-           </div>
-       </Router>
-       </div>
-       
-   );
-  
+        </div>
+      </Router>
+    </div>
+
+  );
+
 }
 
 export default App;
